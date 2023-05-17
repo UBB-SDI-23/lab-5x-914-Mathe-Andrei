decfa37fd15a58380e749e07f029e832212904f4
Mathe Andrei <andrei.mathe@stud.ubbcluj.ro>
2023-05-17 09:59:03 +0300

    Change backend api url

--- react_frontend/src/constants.ts
+++ react_frontend/src/constants.ts
@@ -1,4 +1,4 @@
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api"
 const PROD_BACKEND_API_URL = "https://brainbox.homebrewer.org/api";
 
-export const BACKEND_API_URL = DEV_BACKEND_API_URL;
+export const BACKEND_API_URL = PROD_BACKEND_API_URL;
